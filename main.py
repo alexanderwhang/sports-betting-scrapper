b392e062a3d9af1384dc840d937a1aba7e0c6e8c
Alex Whang <alex@Alexs-MacBook-Pro-5.local>
2023-04-04 15:21:50 -0400

    all live pitcher stats updated

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ path = '/Users/alex/Downloads/chromedriver_mac64/chromedriver'
 driver = webdriver.Chrome(path)
 driver.get(web)
 
-#time.sleep(5)
 sounds_good = driver.find_element('xpath', '/html/body/div[2]/div[3]/div/div/div[3]/button')
 sounds_good.click()
-mlb_live = driver.find_element('xpath', '//*[@id="board"]/div[1]/div/div/div[6]')
+mlb_live = driver.find_element('xpath', '//div[@class="name"][normalize-space()="MLBLIVE"]')
 mlb_live.click()
 props = WebDriverWait(driver, 20).until(
  EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.projection')))
@@ -26,15 +25,15 @@ pitchers = []
 for prop in props:
     name = prop.find_element('xpath', './/div[@class="name"]').text
     team = prop.find_element('xpath', './/div[@class="team-position"]').get_attribute('innerHTML')
+    innings = prop.find_element('xpath', './/div[@class="opponent"]').get_attribute('innerHTML')
     K = prop.find_element('xpath', './/div[@class="presale-score"]').get_attribute('innerHTML')
-    #innings = prop.find_element('xpath', '')
-    #//*[@id="projections"]/div/div/div[2]/div/div[2]/div[2]/div[4]/text()[1]
-    #//*[@id="projections"]/div/div/div[2]/div/div[2]/div[2]/div[4]/text()[2]
-    print(name, K)
+
+    print(name, team, innings, K)
 
     player = {
         'Name': name,
         'Team': team,
+        'Innings': innings,
         'K': K,
         }
 
