9db4c1a29a59f692e1286e8348cc2a7595cd1874
Alex Whang <alexmwhang@gmail.com>
2024-07-07 18:45:59 -0400

    DK setup

---
+++ draftkings/class.py
@@ -0,0 +1,12 @@
+
+
+class DraftKings:
+    def __init__(self, league="MLB"):
+        self.league = league
+    
+    def get_event_ids(self) -> dict:
+        event_ids = {}
+
+        return event_ids
+    
+    
